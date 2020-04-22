5e96a703b03c55bd340bb0d6a9db7df73a3c234d
Michael Fahim <michael_fahim@yahoo.com>
2020-04-22 01:06:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -92,8 +92,8 @@ def can_destroy_a_single_item
   movie.destroy
 
 def can_destroy_all_items_at_once
-  10.times do |i|
-    Movie.create(title: "Movie_#{i}")
+ 10.times do |x|
+    Movie.create(title: "Movie_#{x}")
   end
   Movie.destroy_all
 end
