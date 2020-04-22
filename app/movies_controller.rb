a587225ae1954ee36d8845e33b7233cebd988b1d
Michael Fahim <michael_fahim@yahoo.com>
2020-04-22 01:06:17 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -88,10 +88,8 @@ def can_update_multiple_items_at_once
 end
 
 def can_destroy_a_single_item
-  Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie.destroy
-end
 
 def can_destroy_all_items_at_once
   10.times do |i|
